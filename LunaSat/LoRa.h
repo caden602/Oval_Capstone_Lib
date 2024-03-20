5a922e43b42b3d386f5869f33578e84ba639e08e
Ginn Sato <75648645+ginnsato@users.noreply.github.com>
2024-03-19 19:54:59 -0600

    Delete LunaSat directory

--- LunaSat/LoRa.h
+++
@@ -1,6 +0,0 @@
-#include <RH_RF95_Luna.h>
-
-#define RF95_FREQ   915.0
-
-// LoRa Setup function, params are rf object and rst pin
-void lora_setup(RH_RF95 *rf95, int rst, bool lunasat);
--- LunaSat/scheduler.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-void scheduler_open(void);
-
-uint8_t get_scheduled_events(void);
-
-void add_scheduled_event(uint8_t event);
-
-void remove_scheduled_event(uint8_t event);
